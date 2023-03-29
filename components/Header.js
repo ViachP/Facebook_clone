2103224ff7a6376801dc2e7e9fe4c25d481d69b1
Slava <viachaslau.pailak@gmail.com>
2023-03-29 11:53:01 +0300

    Building the center of the header

--- components/Header.js
+++ components/Header.js
@@ -12,14 +12,13 @@ import {
   FlagIcon,
   PlayIcon,
   MagnifyingGlassIcon,
-  ShoppingCardIcon,
+  ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Header</h1>
-
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -32,12 +31,21 @@ function Header() {
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <MagnifyingGlassIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           />
         </div>
         {/* Center */}
+        <div className="flex justify-center flex-grow">
+          <div className="flex space-x-6 md:space-x-2">
+            <HeaderIcon active Icon={HomeIcon} />
+            <HeaderIcon Icon={FlagIcon} />
+            <HeaderIcon Icon={PlayIcon} />
+            <HeaderIcon Icon={ShoppingCartIcon} />
+            <HeaderIcon Icon={UserGroupIcon} />
+          </div>
+        </div>
         {/* Right */}
       </div>
     </div>
