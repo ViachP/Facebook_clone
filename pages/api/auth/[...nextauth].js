86c02491c1194c9504e8cccac39cf69b7b38c898
Slava <viachaslau.pailak@gmail.com>
2023-04-03 22:23:13 +0300

    Building the Login Component

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth"
-import { SessionProvider } from "next-auth/react";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth( {
   // Configure one or more authentication providers
-  getProviders: [
-    SessionProvider.Facebook({
+  providers: [
+    FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 });
