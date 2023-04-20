378f7d6b3acc851c288c17416371cfcd7a61eaa0
Slava <viachaslau.pailak@gmail.com>
2023-04-20 11:20:49 +0300

    Building the Posts Component

--- components/Feed.js
+++ components/Feed.js
@@ -1,15 +1,14 @@
 import InputBox from "./InputBox"
 import Stories from "./Stories"
+import Posts from "./Posts"
 
 function Feed() {
   return (
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
-        {/* Stories */}
         <Stories />
-        {/* ImputBox */}
         <InputBox />
-        {/* Posts */}
+        <Posts />
         </div>
     </div>
   )
---
+++ components/Post.js
@@ -0,0 +1,8 @@
+
+function Post({name,message,email,postImage,image,timestamp}) {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
