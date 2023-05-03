01e3e37c6a8859557d4307ebbcd5a5358dfab1ee
Slava <viachaslau.pailak@gmail.com>
2023-05-03 21:55:47 +0300

    Debugging Posts and Post components

--- components/Post.js
+++ components/Post.js
@@ -4,26 +4,26 @@ function Post({ name, message, email, postImage, image, timestamp }) {
   return (
     <div className="flex flex-col">
       <div className="p-5 bg-white mt-5 rounded-t-2xl shadow-sm">
-      <div className="flex items-center space-x-2">
-        <Image
-          className="rounded-full"
-          src={image}
-          width={40}
-          height={40}
-          alt="post_image"
-        />
-        <div>
-          <p className="font-medium">{name}</p>
-          <p className="text-xs text-gray-400">
-            {new Date(timestamp?.toDate()).toLocaleString()}
-          </p>
+        <div className="flex items-center space-x-2">
+          <Image
+            className="rounded-full"
+            src={image}
+            width={40}
+            height={40}
+            alt="post_image"
+          />
+          <div>
+            <p className="font-medium">{name}</p>
+            <p className="text-xs text-gray-400">
+              {new Date(timestamp?.toDate()).toLocaleString()}
+            </p>
+          </div>
         </div>
-      </div>
-      <p className="pt-4">{message}</p>
+        <p className="pt-4">{message}</p>
       </div>
       {postImage && (
         <div>
-          <Image src={postImage}  alt=""/>
+          <Image src={postImage} width={300} height={200} alt="" />
         </div>
       )}
     </div>
--- components/Posts.js
+++ components/Posts.js
@@ -1,30 +1,29 @@
-import {useCollection} from "react-firebase-hooks/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
+import "firebase/compat/firestore";
 import { db } from "../firebase";
-import 'firebase/compat/firestore'
 import Post from "./Post";
 
 function Posts() {
+  const [realtimePosts] = useCollection(
+    db.collection("posts").orderBy("timestamp", "desc")
+  );
 
-    const [realtimePosts] = useCollection(
-        db.collection('posts').orderBy('timestamp','desc')
-    );
-    console.log(realtimePosts)
-    
   return (
     <div>
-        {realtimePosts?.docs.map(post => {
-            <Post 
-            key={post.id}
-            name={post.data().name}
-            message={post.data().message}
-            email={post.data().email}
-            timestamp={post.data().timestamp}
-            image={post.data().image}
-            postImage={post.data().postImage}
-            />
-        })}
+      {realtimePosts?.docs.map((post) => {
+        return(
+        <Post
+          key={post.id}
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />);
+      })}
     </div>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
